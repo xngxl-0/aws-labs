cae9cd6b4fb0e9d57bd2d8aead9daeb7266185df
xngxl-0 <engeldavidtapia@gmail.com>
2026-07-30 18:23:39 +0200

    Añadido contenido de terraform para subir archivos a s3

---
+++ labs/01-static-website/terraform/main.tf
@@ -0,0 +1,53 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Herramienta = "Terraform"
+    Lab         = "Lab01EngelTapia"
+  }
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "index.html"
+  source       = "../website/index.html"
+  etag         = filemd5("../website/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "error.html"
+  source       = "../website/error.html"
+  etag         = filemd5("../website/error.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "style" {
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "css/style.css"
+  source       = "../website/css/style.css"
+  etag         = filemd5("../website/css/style.css")
+  content_type = "text/css"
+}
+
+
+resource "aws_s3_object" "javascript" {
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "js/script.js"
+  source       = "../website/js/script.js"
+  etag         = filemd5("../website/js/script.js")
+  content_type = "application/javascript"
+}
+
+
+###Imagenes que se suben al bucket con for-each###
+resource "aws_s3_object" "images" {
+  for_each     = fileset("../Imagenes/", "*.png")
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "imagenes/${each.value}"
+  source       = "../Imagenes/${each.value}"
+  etag         = filemd5("../Imagenes/${each.value}")
+  content_type = "image/png"
+}   
+
---
+++ labs/01-static-website/terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "bucket_name" {
+  description = "Nombre del Bucket a crear en S3"
+  type        = string
+  default    = "engeltapia-lab01-website-terraform-2026"
+}
